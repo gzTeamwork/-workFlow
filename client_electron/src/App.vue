c61d9ab0555340fd3530204abb0d2b305e612c8c
Zico <21520993@qqcom>
2019-09-12 01:53:48 +0800

    增加iview框架

--- client_electron/src/App.vue
+++ client_electron/src/App.vue
@@ -2,9 +2,22 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <Icon type="ios-alert" />
     </div>
     <router-view/>
+    <Button type="primary" shape="circle" icon="ios-search"></Button>
+    <Button type="primary" icon="ios-search">Search</Button>
+    <Button type="primary" shape="circle" icon="ios-search">Search</Button>
+    <Button type="primary" shape="circle">Circle</Button>
+    <br><br>
+    <Button shape="circle" icon="ios-search"></Button>
+    <Button icon="ios-search">Search</Button>
+    <Button shape="circle" icon="ios-search">Search</Button>
+    <Button shape="circle">Circle</Button>
+    <p>
+      Using Iview UI Framework!
+    </p>
   </div>
 </template>
 <style lang="stylus">
--- client_electron/src/main.ts
+++ client_electron/src/main.ts
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Routers from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
+Vue.use(iView);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router:Routers,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
